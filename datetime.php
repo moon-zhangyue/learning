a6e1593fd57f52745131196a8b1ba96f54e5ce5e
moon-zhangyue <575865770@qq.com>
2022-01-16 16:34:52 +0800

    datetime函数

---
+++ datetime.php
@@ -0,0 +1,23 @@
+<?php
+
+$raw   = '22. 11. 1968';
+$start = DateTime::createFromFormat('d. m. Y', $raw);
+
+echo 'Start date: ' . $start->format('Y-m-d') . "\n";
+
+// create a copy of $start and add one month and 6 days
+$end = clone $start;
+$end->add(new DateInterval('P1M6D'));
+
+$diff = $end->diff($start);
+echo 'Difference: ' . $diff->format('%m month, %d days (total: %a days)') . "\n";
+// Difference: 1 month, 6 days (total: 37 days)
+
+
+// output all thursdays between $start and $end
+$periodInterval = DateInterval::createFromDateString('first thursday');
+$periodIterator = new DatePeriod($start, $periodInterval, $end, DatePeriod::EXCLUDE_START_DATE);
+foreach ($periodIterator as $date) {
+    // output each date in the period
+    echo $date->format('Y-m-d') . ' ';
+}
