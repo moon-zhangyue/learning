72c3668e1f8d3fc6ddaf3b50c8efb33683032b58
moon-zhangyue <575865770@qq.com>
2022-01-20 15:34:31 +0800

    commit

---
+++ php8.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * 定义一个支持联合类型的 Number 类
+ */
+class Number
+{
+private int|float $number;
+
+    public function setNumber(int|float $number): void {
+        $this->number = $number;
+    }
+
+public
+function getNumber(): int|float {
+    return $this->number;
+}
+}
+
+/**
+ * 我们可以传递浮点型和整型值到 Number 对象
+ */
+$number = new Number();
+
+$number->setNumber(5);
+
+var_dump($number->getNumber());
+
+$number->setNumber(11.54);
+
+var_dump($number->getNumber());
+
+exit;
