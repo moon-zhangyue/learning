4669d83cc6821c46e075aa11191a6cf111edbbfe
moon-zhangyue <575865770@qq.com>
2022-01-30 14:45:35 +0800

    refactor: 简短PHP 函数

---
+++ 30_second_code.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2022/1/30 下午2:44
+ * Module: 30_second_code.php
+ * Please No Garbage Code!
+ */
+
+//排列
+//all
+//如果所提供的函数返回 true 的数量等于数组中成员数量的总和，则函数返回 true，否则返回 false。
+
+function all($items, $func)
+{
+    return count(array_filter($items, $func)) === count($items);
+}
