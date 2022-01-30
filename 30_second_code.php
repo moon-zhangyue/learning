749fa3b3f1510d42b325bc49d35dd0e199872d59
moon-zhangyue <575865770@qq.com>
2022-01-30 14:50:26 +0800

    refactor: 简短PHP 函数

--- 30_second_code.php
+++ 30_second_code.php
@@ -10,8 +10,12 @@
 //排列
 //all
 //如果所提供的函数返回 true 的数量等于数组中成员数量的总和，则函数返回 true，否则返回 false。
-
 function all($items, $func)
 {
     return count(array_filter($items, $func)) === count($items);
 }
+
+//Examples
+all([2, 3, 4, 5], function ($item) {
+    return $item > 1;
+}); // true
