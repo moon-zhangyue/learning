ffb6a08de82da6ef02974ee5e4acc3cc3400a0c0
moon-zhangyue <575865770@qq.com>
2022-02-01 16:22:14 +0800

    refactor:简短PHP 函数 without

--- 30_second_code.php
+++ 30_second_code.php
@@ -90,3 +90,114 @@ findLastIndex([1, 2, 3, 4], function ($n) {
     return ($n % 2) === 1;
 });
 // 2
+
+//groupBy 根据给定的函数对数组的元素进行分组。
+function groupBy($items, $func)
+{
+    $group = [];
+    foreach ($items as $item) {
+        if ((!is_string($func) && is_callable($func)) || function_exists($func)) {
+            $key           = call_user_func($func, $item);
+            $group[$key][] = $item;
+        } elseif (is_object($item)) {
+            $group[$item->{$func}][] = $item;
+        } elseif (isset($item[$func])) {
+            $group[$item[$func]][] = $item;
+        }
+    }
+
+    return $group;
+}
+
+var_dump(groupBy(['one', 'two', 'three'], 'strlen')); // [3 => ['one', 'two'], 5 => ['three']]
+
+//pluck检索给定键名的所有键值
+function pluck($items, $key): array
+{
+    return array_map(function ($item) use ($key) {
+        return is_object($item) ? $item->$key : $item[$key];
+    }, $items);
+}
+
+pluck([
+    ['product_id' => 'prod-100', 'name' => 'Desk'],
+    ['product_id' => 'prod-200', 'name' => 'Chair'],
+], 'name');
+// ['Desk', 'Chair']
+
+//pull修改原始数组以过滤掉指定的值。
+function pull(&$items, ...$params): array
+{
+    $items = array_values(array_diff($items, $params));
+    return $items;
+}
+
+$items = ['a', 'b', 'c', 'a', 'b', 'c'];
+pull($items, 'a', 'c'); // $items will be ['b', 'b']
+
+
+//reject使用给定的回调筛选数组。
+function reject($items, $func): array
+{
+    return array_values(array_diff($items, array_filter($items, $func)));
+}
+
+reject(['Apple', 'Pear', 'Kiwi', 'Banana'], function ($item) {
+    return strlen($item) > 4;
+}); // ['Pear', 'Kiwi']
+
+//remove从给定函数返回false的数组中删除元素。
+function remove($items, $func): array
+{
+    $filtered = array_filter($items, $func);
+
+    return array_diff_key($items, $filtered);
+}
+
+remove([1, 2, 3, 4], function ($n) {
+    return ($n % 2) === 0;
+});
+// [0 => 1, 2 => 3]
+
+//take返回一个数组，其中从开头删除了n个元素。
+function take($items, $n = 1): array
+{
+    return array_slice($items, 0, $n);
+}
+
+take([1, 2, 3], 5); // [1, 2, 3]
+take([1, 2, 3, 4, 5], 2); // [1, 2]
+
+//without筛选出给定值之外的数组元素。
+function without($items, ...$params): array
+{
+    return array_values(array_diff($items, $params));
+}
+without([2, 1, 2, 3, 5, 8], 1, 2, 8); // [3, 5]
+
+//orderBy按键名对数组或对象的集合进行排序。
+function orderBy($items, $attr, $order): array
+{
+    $sortedItems = [];
+    foreach ($items as $item) {
+        $key = is_object($item) ? $item->{$attr} : $item[$attr];
+        $sortedItems[$key] = $item;
+    }
+    if ($order === 'desc') {
+        krsort($sortedItems);
+    } else {
+        ksort($sortedItems);
+    }
+
+    return array_values($sortedItems);
+}
+
+orderBy(
+    [
+        ['id' => 2, 'name' => 'Joy'],
+        ['id' => 3, 'name' => 'Khaja'],
+        ['id' => 1, 'name' => 'Raja']
+    ],
+    'id',
+    'desc'
+); // [['id' => 3, 'name' => 'Khaja'], ['id' => 2, 'name' => 'Joy'], ['id' => 1, 'name
