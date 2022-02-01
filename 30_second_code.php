a2ae2d4c9757416651858ee6d4bab8aedbffda5f
moon-zhangyue <575865770@qq.com>
2022-02-01 11:57:27 +0800

    refactor: 简短PHP 函数 将多维数组 转为一维数组

--- 30_second_code.php
+++ 30_second_code.php
@@ -33,3 +33,22 @@ function any($items, $func)
 any([1, 2, 3, 4], function ($item) {
     return $item < 2;
 }); // true
+
+
+/*deepFlatten(深度平铺数组)
+将多维数组转为一维数组*/
+function deepFlatten($items): array
+{
+    $result = [];
+    foreach ($items as $item) {
+        if (!is_array($item)) {
+            $result[] = $item;
+        } else {
+            $result = array_merge($result, deepFlatten($item));
+        }
+    }
+
+    return $result;
+}
+
+var_dump(deepFlatten([1, [2], [[3], 4], 5])); // [1, 2, 3, 4, 5]
