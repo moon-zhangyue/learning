74dfc3b63c3d3d6c189b69a3d97f7ad6df8411bb
moon-zhangyue <575865770@qq.com>
2022-02-06 20:54:10 +0800

    &and&&

--- code.php
+++ code.php
@@ -13,15 +13,59 @@
  * */
 if (($a = 1) & ($a == 1) & ($a = 3)) {
     echo 'true', $a;
-    die;
+} else {
+    echo 'false', $a;
 }
-echo 'false', $a;
 
 /*
  * 输出：false3
 简析：即使前面已经为 false, 后面的表达式依旧会执行。
  * */
 if (false & ($a = 3)) {
-    echo 'true', $a;die;
+    echo 'true', $a;
+} else {
+    echo 'false', $a;
 }
-echo 'false', $a;
+
+
+if (true & ($a = 1)) {
+    echo 'true', $a;
+} else {
+    echo 'false', $a;
+}
+//输出：true1
+
+if (true & ($a = 2)) {
+    echo 'true', $a;
+} else {
+    echo 'false', $a;
+}
+//输出：false2
+
+if (true & ($a = 3)) {
+    echo 'true', $a;
+} else {
+    echo 'false', $a;
+}
+//输出：true3
+
+if (true & ($a = 4)) {
+    echo 'true', $a;
+} else {
+    echo 'false', $a;
+}
+//输出：false4
+
+/*简析：一个 & 是按位“与”。
+true 会转换成 1。
+数字转换成二进制：
+1 -> 1
+2 -> 10
+3 -> 11
+4 -> 100
+
+所以：
+1 & 1 => 1 => true
+1 & 10 => 0 => false
+1 & 11 => 1 => true
+1 & 100 => 0 => false*/
