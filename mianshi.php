3deb629044a090459425d7e3673e07e387bb4db7
moon-zhangyue <575865770@qq.com>
2022-02-09 15:19:23 +0800

    refactor:array +

---
+++ mianshi.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2022/2/9 下午3:16
+ * Module: mianshi.php
+ * Please No Garbage Code!
+ */
+
+$a = [0, 1, 2, 3];
+$b = [1, 2, 3, 4, 5];
+$a += $b;
+echo json_encode($a);
+
+// + 会把$b中 $a已存在的键全部忽略
