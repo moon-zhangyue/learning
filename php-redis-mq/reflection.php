aacb038b2bef0c4f9272d38c6ca5352a9ec5c450
moon-zhangyue <575865770@qq.com>
2022-02-16 22:55:23 +0800

    refactor: 反射API

---
+++ php-redis-mq/reflection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2022/2/16 下午10:50
+ * Module: reflection.php
+ * 反射
+ */
+
+class person
+{
+    public string $name;
+    public string $gender;
+
+    public function say()
+    {
+        echo $this->name, " \tis ", $this->gender, "\r\n";
+    }
+
+    public function set($name, $value)
+    {
+        echo "Setting $name to $value \r\n";
+        $this->$name = $value;
+    }
+
+    public function get($name)
+    {
+        if (!isset($this->$name)) {
+            echo '未设置';
+            $this->$name = "正在为你设置默认值";
+        }
+        return $this->$name;
+    }
+}
+
+$student         = new person();
+$student->name   = 'Tom';
+$student->gender = 'male';
+$student->age    = 24;
