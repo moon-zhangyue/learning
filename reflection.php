d9da5cca6b53fc7310b7acb9fc476192176ce4bc
moon-zhangyue <575865770@qq.com>
2022-02-17 21:19:06 +0800

    refactor: 反射

--- reflection.php
+++ reflection.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * Author: SpiRit-Moon
@@ -50,3 +51,50 @@ $m = $reflect->getMethods();
 foreach ($m as $prop) {
     print $prop->getName() . "\n";
 }
+
+// 返回对象属性的关联数组
+var_dump(get_object_vars($student));
+// 类属性
+var_dump(get_class($student));
+var_dump(get_class_vars(get_class($student)));
+// 返回由类的方法名组成的数组
+var_dump(get_class_methods(get_class($student)));
+
+$obj       = new ReflectionClass('person');
+$className = $obj->getName();
+$Methods   = $Properties = array();
+foreach ($obj->getProperties() as $v) {
+    $Properties[$v->getName()] = $v;
+}
+foreach ($obj->getMethods() as $v) {
+    $Methods[$v->getName()] = $v;
+}
+echo "class {$className}\n{\n";
+is_array($Properties) && ksort($Properties);
+foreach ($Properties as $k => $v) {
+    echo "\t";
+    echo $v->isPublic() ? ' public' : '', $v->isPrivate() ? ' private' : '',
+    $v->isProtected() ? ' protected' : '',
+    $v->isStatic() ? ' static' : '';
+    echo "\t{$k}\n";
+}
+echo "\n";
+if (is_array($Methods)) ksort($Methods);
+foreach ($Methods as $k => $v) {
+    echo "\tfunction {$k}(){}\n";
+}
+echo "}\n";
+
+//输出如下
+/*class person
+{
+	public gender
+	public name
+	function get(){}
+	function set(){}
+	function say(){}
+}*/
+
+//## 反射有什么作用
+//反射可以用于文档生成。因此可以用它对文件里的类进行扫描，逐个生成描述文档。
+//既然反射可以探知类的内部结构，那么是不是可以用它做hook实现插件功能呢？或者是做动态代理呢？
