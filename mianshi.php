29ea5593339f37fce9a1ca2a16e05ae67b956107
moon-zhangyue <575865770@qq.com>
2022-02-17 20:54:11 +0800

    refactor: 面试题

--- mianshi.php
+++ mianshi.php
@@ -33,3 +33,40 @@ foreach ($a as $v) {
 
 }
 echo json_encode($a);
+
+/*
+ * 将一个或多个数组的单元合并起来，一个数组中的值附加在前一个数组的后面。返回作为结果的数组。
+如果输入的数组中有相同的字符串键名，则该键名后面的值将覆盖前一个值。然而，如果数组包含数字键名，后面的值将 不会 覆盖原来的值，而是附加到后面。
+如果输入的数组存在以数字作为索引的内容，则这项内容的键名会以连续方式重新索引
+ * */
+$a = ['10022' => 'mm', '10023' => 'dd'];
+$b = ['p' => 1, 'q' => 2];
+
+var_dump(array_merge($a, $b));
+
+class person
+{
+    public string $lan;
+
+    public function __construct($lan)
+    {
+        $this->lan = $lan;
+    }
+
+    public function speak()
+    {
+        echo $this->lan;
+    }
+}
+
+$person = new person('中文');
+$person->speak();
+
+$person2      = $person;
+$person2->lan = '英文';
+
+$person2->speak();
+$person->speak();
+
+
+
