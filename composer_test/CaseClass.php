310e9a1257bdee60fe1d69b670e5a05cd89bee95
zhangyue <575865770@qq.com>
2022-02-23 17:27:24 +0800

    refactor: spl_autoload_register  自动加载

---
+++ composer_test/CaseClass.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2022/2/23 17:19
+ * Module: CaseClass.php
+ */
+
+spl_autoload_register(function ($className) {
+    require_once 'TestClass' . '.php';
+});
+
+
+spl_autoload_register('CaseAutoLoad');
+function CaseAutoLoad($className)
+{
+    echo "second==>\n";
+    require_once 'CaseClass.php';
+}
+
+
+$testClass = new TestClass();
+$testClass->print();
+
+echo "还没实例化 CaseClass \n";
+
+// 在这之前都没有执行第二个 spl_autoload_register函数 也就是利用闭包实现了懒加载
+
+$s = new CaseClass();
+$s->show();
