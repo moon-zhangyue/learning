222dbfe9c2cbd7c3a929cd34ce55b0a9c33f773f
zhangyue <575865770@qq.com>
2022-02-28 20:25:28 +0800

    refactor: trait 使用

--- trait.php
+++ trait.php
@@ -7,7 +7,7 @@
  */
 
 
-trait Hello
+/*trait Hello
 {
     public function sayHello()
     {
@@ -36,4 +36,60 @@ class MyHelloWorld
 $o = new MyHelloWorld();
 $o->sayHello();
 $o->sayWorld();
-$o->sayExclamationMark();
+$o->sayExclamationMark();*/
+
+
+trait Cat
+{
+    public function eat()
+    {
+        echo "This is Cat eat";
+    }
+}
+
+trait Dog
+{
+    use Cat;
+
+    public function drive()
+    {
+        echo "This is Dog drive";
+    }
+
+    abstract public function getName();
+
+    public function test()
+    {
+        static $num = 0;
+        $num++;
+        echo $num;
+    }
+
+    public static function say()
+    {
+        echo "This is Dog say";
+    }
+}
+
+class animal
+{
+    use Dog;
+
+    public function getName()
+    {
+        echo "This is animal name";
+    }
+}
+
+$animal = new animal();
+$animal->getName();
+echo "<br/>";
+$animal->eat();
+echo "<br/>";
+$animal->drive();
+echo "<br/>";
+$animal::say();
+echo "<br/>";
+$animal->test();
+echo "<br/>";
+$animal->test();
