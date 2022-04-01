b6e4e53c7905e49f7d1e43bb9f14abee6b1e3135
zhangyue <575865770@qq.com>
2022-04-01 16:27:58 +0800

    refactor: 常用函数

---
+++ common.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2022/4/1 16:14
+ * Module: common.php
+ */
+
+$substr    = substr('henghenghaha', 0, 5);
+$cn_substr = substr('这是字符串', 0, 5);
+var_dump($substr);
+var_dump($cn_substr);
