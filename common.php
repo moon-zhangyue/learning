0a18b76097edf5b2c19903d62bbf415309f3373b
zhangyue <575865770@qq.com>
2022-04-01 17:00:27 +0800

    refactor: 常用函数

--- common.php
+++ common.php
@@ -7,6 +7,9 @@
  */
 
 $substr    = substr('henghenghaha', 0, 5);
-$cn_substr = substr('这是字符串', 0, 5);
+$cn_substr = substr('这是字符串', 0, 6);
 var_dump($substr);
 var_dump($cn_substr);
+
+$bodytag = str_replace("%body%", "black", "<body text='%body%'>");
+echo $bodytag;
