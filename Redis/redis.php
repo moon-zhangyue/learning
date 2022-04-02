89044893aea99b59e7eacc26e1534bbf4c574318
zhangyue <575865770@qq.com>
2022-04-02 14:27:22 +0800

    refactor: redis操作set

--- Redis/redis.php
+++ Redis/redis.php
@@ -16,3 +16,17 @@ if (!$redis->connect('127.0.0.1', 6379)) {
 
 $data = $redis->keys('*');
 var_dump($data);
+
+//set操作
+//$res = $redis->hSet('user1', 'age', 26);
+//$res = $redis->hSet('user1', 'name', 'hahaha');
+//var_dump($res);
+
+$res = $redis->hGet('user1', 'age');
+var_dump($res);
+
+var_dump($redis->hGetAll('user1')); //返回hash key对应所有的field和value
+var_dump($redis->hVals('user1')); //返回hash key对应所有的field的value 没有field
+var_dump($redis->hExists('user1', 'name'));
+var_dump($redis->hLen('user1')); //	获取hash key field的数量
+var_dump($redis->hIncrBy('user1', 'age', 2));
