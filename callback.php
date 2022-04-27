8869be3dfedeb39f7b05356b6833c96efad87e01
zhangyue <575865770@qq.com>
2022-04-27 20:34:03 +0800

    refactor: 匿名函数的四种方法

---
+++ callback.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2022/4/27 20:29
+ * Module: callback.php
+ */
+
+/*回调函数*/
+
+//函数字符串
+/*function insert(int $i): bool
+{
+    echo "插入数据{$i}\n";//模拟数据库插入
+    return true;
+}
+
+$arr = range(0, 1000);//模拟生成1001条数据
+function action(array $arr, callable $function)
+{
+    foreach ($arr as $value) {
+        if ($value % 10 == 0) {//当满足条件时,去执行回调函数处理
+            call_user_func($function, $value);
+        }
+    }
+}
+
+action($arr, 'insert');*/
+
+//匿名函数
+/*$arr = range(0, 1000);//模拟生成1001条数据
+function action(array $arr, callable $function)
+{
+    foreach ($arr as $value) {
+        if ($value % 10 == 0) {//当满足条件时,去执行回调函数处理
+            call_user_func($function, $value);
+        }
+    }
+}
+
+action($arr, function ($i) {
+    echo "插入数据{$i}\n";//模拟数据库插入
+    return true;
+});*/
+
+//类静态方法
+/*$arr = range(0, 100);//模拟生成1001条数据
+function action(array $arr, callable $function)
+{
+    foreach ($arr as $value) {
+        if ($value % 10 == 0) {//当满足条件时,去执行回调函数处理
+            call_user_func($function, $value);
+        }
+    }
+}
+
+class A
+{
+    static function insert(int $i): bool
+    {
+        echo "插入数据{$i}\n";//模拟数据库插入
+        return true;
+    }
+}
+
+action($arr, 'A::insert');
+action($arr, array('A', 'insert'));*/
+
+//类方法
+$arr = range(0, 100);//模拟生成1001条数据
+function action(array $arr, callable $function)
+{
+    foreach ($arr as $value) {
+        if ($value % 10 == 0) {//当满足条件时,去执行回调函数处理
+            call_user_func($function, $value);
+        }
+    }
+}
+
+class A
+{
+    public function insert(int $i): bool
+    {
+        echo "插入数据{$i}\n";//模拟数据库插入
+        return true;
+    }
+}
+
+$a = new A();
+action($arr, array($a, 'insert'));
